2b7dd7b554abc4a7201a2bbfe1a0ab26df8f436e
Kenneth Corrales <kcorrales33@gmail.com>
2019-09-18 18:21:44 +0200

    termine la ventanta sign Up

--- src/Frames/SignUp.java
+++ src/Frames/SignUp.java
@@ -19,6 +19,27 @@ public class SignUp extends javax.swing.JFrame {
     public SignUp() {
         initComponents();
         jPanel1.setBackground(new Color(0,0,0,80));
+        txtCedula.setBackground(new Color(102,0,102,80));
+        txtName.setBackground(new Color(102,0,102,80));
+        txtEmail.setBackground(new Color(102,0,102,80));
+        
+        this.txtFirstLastName.setBackground(new Color(102,0,102,80));
+        this.txtPassw.setBackground(new Color(102,0,102,80));
+        this.txtPasswConfirm.setBackground(new Color(102,0,102,80));
+        this.txtSecondLastName.setBackground(new Color(102,0,102,80));
+        this.txtTelefono.setBackground(new Color(102,0,102,80));
+        this.txtUser.setBackground(new Color(102,0,102,80));
+        this.comboNacionalidad.setBackground(new Color(102,0,102,80));
+        this.fechaNacimiento.setBackground(new Color(102,0,102,80));
+        
+        this.txtCedula.setForeground(new Color(222,222,222));
+        this.txtPassw.setForeground(new Color(222,222,222));
+        this.txtPasswConfirm.setForeground(new Color(222,222,222));
+        this.txtEmail.setForeground(new Color(222,222,222));
+        this.txtUser.setForeground(new Color(222,222,222));
+        this.txtSecondLastName.setForeground(new Color(222,222,222));
+        
+        
     }
 
     /**
@@ -35,24 +56,31 @@ public class SignUp extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
-        jPasswordField1 = new javax.swing.JPasswordField();
         kButton1 = new keeptoo.KButton();
         jLabel5 = new javax.swing.JLabel();
-        jPasswordField2 = new javax.swing.JPasswordField();
+        txtPasswConfirm = new javax.swing.JPasswordField();
         jLabel6 = new javax.swing.JLabel();
         jLabel7 = new javax.swing.JLabel();
-        jTextField2 = new javax.swing.JTextField();
-        jTextField3 = new javax.swing.JTextField();
-        jDateChooser1 = new com.toedter.calendar.JDateChooser();
+        txtEmail = new javax.swing.JTextField();
+        fechaNacimiento = new com.toedter.calendar.JDateChooser();
         jLabel8 = new javax.swing.JLabel();
         jLabel9 = new javax.swing.JLabel();
-        jTextField4 = new javax.swing.JTextField();
-        jComboBox1 = new javax.swing.JComboBox<>();
+        txtSecondLastName = new javax.swing.JTextField();
+        comboNacionalidad = new javax.swing.JComboBox<>();
         jLabel10 = new javax.swing.JLabel();
-        jTextField5 = new javax.swing.JTextField();
+        txtCedula = new javax.swing.JTextField();
         jLabel11 = new javax.swing.JLabel();
+        jLabel12 = new javax.swing.JLabel();
+        txtPassw = new javax.swing.JPasswordField();
+        btnFoto = new keeptoo.KButton();
+        jLabel13 = new javax.swing.JLabel();
+        txtTelefono = new javax.swing.JTextField();
+        txtFirstLastName = new javax.swing.JTextField();
+        txtName = new javax.swing.JTextField();
+        txtUser = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
 
         kGradientPanel1.setkEndColor(new java.awt.Color(51, 51, 255));
         kGradientPanel1.setkStartColor(new java.awt.Color(255, 0, 204));
@@ -64,8 +92,8 @@ public class SignUp extends javax.swing.JFrame {
 
         jLabel2.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         jLabel2.setForeground(new java.awt.Color(255, 0, 204));
-        jLabel2.setText("Apellidos");
-        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 160, 250, 30));
+        jLabel2.setText("Primer Apellido");
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 240, 250, 30));
 
         jLabel3.setFont(new java.awt.Font("Segoe UI", 1, 36)); // NOI18N
         jLabel3.setForeground(new java.awt.Color(255, 0, 204));
@@ -74,88 +102,140 @@ public class SignUp extends javax.swing.JFrame {
 
         jLabel4.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         jLabel4.setForeground(new java.awt.Color(255, 0, 204));
-        jLabel4.setText("Contraseña");
-        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 360, 250, 20));
-
-        jPasswordField1.setBackground(new java.awt.Color(204, 255, 204));
-        jPasswordField1.setFont(new java.awt.Font("Segoe UI", 0, 12)); // NOI18N
-        jPasswordField1.setBorder(javax.swing.BorderFactory.createMatteBorder(0, 0, 2, 0, new java.awt.Color(51, 51, 255)));
-        jPanel1.add(jPasswordField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 190, 250, 40));
+        jLabel4.setText("Confirmar Contraseña");
+        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(740, 350, 250, 20));
 
         kButton1.setText("ENTRAR");
         kButton1.setBorderPainted(false);
-        jPanel1.add(kButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(440, 580, -1, -1));
+        jPanel1.add(kButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 580, -1, -1));
 
         jLabel5.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         jLabel5.setForeground(new java.awt.Color(255, 0, 204));
         jLabel5.setText("Email");
-        jPanel1.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(750, 250, 250, 30));
+        jPanel1.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 160, 250, 30));
 
-        jPasswordField2.setBackground(new java.awt.Color(204, 255, 204));
-        jPasswordField2.setFont(new java.awt.Font("Segoe UI", 0, 12)); // NOI18N
-        jPasswordField2.setBorder(javax.swing.BorderFactory.createMatteBorder(0, 0, 2, 0, new java.awt.Color(51, 51, 255)));
-        jPanel1.add(jPasswordField2, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 430, 250, 40));
+        txtPasswConfirm.setBackground(new java.awt.Color(204, 255, 204));
+        txtPasswConfirm.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        txtPasswConfirm.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        txtPasswConfirm.setBorder(javax.swing.BorderFactory.createMatteBorder(0, 0, 2, 0, new java.awt.Color(51, 51, 255)));
+        jPanel1.add(txtPasswConfirm, new org.netbeans.lib.awtextra.AbsoluteConstraints(740, 380, 250, 40));
 
         jLabel6.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         jLabel6.setForeground(new java.awt.Color(255, 0, 204));
         jLabel6.setText("Nombre");
-        jPanel1.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 90, 250, 20));
+        jPanel1.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 160, 250, 20));
 
         jLabel7.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         jLabel7.setForeground(new java.awt.Color(255, 0, 204));
         jLabel7.setText("Nacionalidad");
-        jPanel1.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 240, 250, 20));
+        jPanel1.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 430, 250, 20));
 
-        jTextField2.setBackground(new java.awt.Color(204, 255, 204));
-        jTextField2.setFont(new java.awt.Font("Segoe UI", 0, 12)); // NOI18N
-        jTextField2.setBorder(javax.swing.BorderFactory.createMatteBorder(0, 0, 2, 0, new java.awt.Color(51, 51, 255)));
-        jPanel1.add(jTextField2, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 120, 250, 40));
+        txtEmail.setBackground(new java.awt.Color(204, 255, 204));
+        txtEmail.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        txtEmail.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        txtEmail.setBorder(javax.swing.BorderFactory.createMatteBorder(0, 0, 2, 0, new java.awt.Color(51, 51, 255)));
+        jPanel1.add(txtEmail, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 190, 250, 40));
 
-        jTextField3.setBackground(new java.awt.Color(204, 255, 204));
-        jTextField3.setFont(new java.awt.Font("Segoe UI", 0, 12)); // NOI18N
-        jTextField3.setBorder(javax.swing.BorderFactory.createMatteBorder(0, 0, 2, 0, new java.awt.Color(51, 51, 255)));
-        jPanel1.add(jTextField3, new org.netbeans.lib.awtextra.AbsoluteConstraints(750, 290, 250, 40));
-
-        jDateChooser1.setBackground(new java.awt.Color(204, 255, 204));
-        jDateChooser1.setForeground(new java.awt.Color(204, 255, 204));
-        jDateChooser1.setDateFormatString("dd/MM/yyyy");
-        jDateChooser1.setFont(new java.awt.Font("Segoe UI", 0, 12)); // NOI18N
-        jPanel1.add(jDateChooser1, new org.netbeans.lib.awtextra.AbsoluteConstraints(550, 180, -1, 20));
+        fechaNacimiento.setBackground(new java.awt.Color(204, 255, 204));
+        fechaNacimiento.setForeground(new java.awt.Color(204, 255, 204));
+        fechaNacimiento.setDateFormatString("dd/MM/yyyy");
+        fechaNacimiento.setFont(new java.awt.Font("Segoe UI", 0, 12)); // NOI18N
+        jPanel1.add(fechaNacimiento, new org.netbeans.lib.awtextra.AbsoluteConstraints(520, 370, -1, 20));
 
         jLabel8.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         jLabel8.setForeground(new java.awt.Color(255, 0, 204));
         jLabel8.setText("Telefono");
-        jPanel1.add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 90, 250, 20));
+        jPanel1.add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 430, 250, 20));
 
         jLabel9.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         jLabel9.setForeground(new java.awt.Color(255, 0, 204));
         jLabel9.setText("Fecha de Nacimiento");
-        jPanel1.add(jLabel9, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 180, 250, 20));
+        jPanel1.add(jLabel9, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 340, 250, 20));
 
-        jTextField4.setBackground(new java.awt.Color(204, 255, 204));
-        jTextField4.setFont(new java.awt.Font("Segoe UI", 0, 12)); // NOI18N
-        jTextField4.setBorder(javax.swing.BorderFactory.createMatteBorder(0, 0, 2, 0, new java.awt.Color(51, 51, 255)));
-        jPanel1.add(jTextField4, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 120, 250, 40));
+        txtSecondLastName.setBackground(new java.awt.Color(204, 255, 204));
+        txtSecondLastName.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        txtSecondLastName.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        txtSecondLastName.setBorder(javax.swing.BorderFactory.createMatteBorder(0, 0, 2, 0, new java.awt.Color(51, 51, 255)));
+        jPanel1.add(txtSecondLastName, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 370, 250, 40));
 
-        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
-        jPanel1.add(jComboBox1, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 270, 240, 30));
+        comboNacionalidad.setBackground(new java.awt.Color(204, 255, 204));
+        comboNacionalidad.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        comboNacionalidad.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Costa Rica", "Mexico", "Cuba", "Haiti" }));
+        comboNacionalidad.setBorder(javax.swing.BorderFactory.createMatteBorder(0, 0, 0, 0, new java.awt.Color(0, 0, 0)));
+        jPanel1.add(comboNacionalidad, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 470, 250, 30));
 
         jLabel10.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         jLabel10.setForeground(new java.awt.Color(255, 0, 204));
         jLabel10.setText("Cédula");
-        jPanel1.add(jLabel10, new org.netbeans.lib.awtextra.AbsoluteConstraints(750, 130, 250, 30));
+        jPanel1.add(jLabel10, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 240, 250, 30));
 
-        jTextField5.setBackground(new java.awt.Color(204, 255, 204));
-        jTextField5.setFont(new java.awt.Font("Segoe UI", 0, 12)); // NOI18N
-        jTextField5.setBorder(javax.swing.BorderFactory.createMatteBorder(0, 0, 2, 0, new java.awt.Color(51, 51, 255)));
-        jPanel1.add(jTextField5, new org.netbeans.lib.awtextra.AbsoluteConstraints(770, 150, 250, 40));
+        txtCedula.setBackground(new java.awt.Color(102, 0, 102));
+        txtCedula.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        txtCedula.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        txtCedula.setBorder(null);
+        jPanel1.add(txtCedula, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 270, 250, 40));
 
         jLabel11.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         jLabel11.setForeground(new java.awt.Color(255, 0, 204));
         jLabel11.setText("Usuario");
-        jPanel1.add(jLabel11, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 260, 250, 20));
+        jPanel1.add(jLabel11, new org.netbeans.lib.awtextra.AbsoluteConstraints(740, 160, 250, 20));
 
-        kGradientPanel1.add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 60, 1050, 680));
+        jLabel12.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jLabel12.setForeground(new java.awt.Color(255, 0, 204));
+        jLabel12.setText("Contraseña");
+        jPanel1.add(jLabel12, new org.netbeans.lib.awtextra.AbsoluteConstraints(740, 260, 250, 20));
+
+        txtPassw.setBackground(new java.awt.Color(204, 255, 204));
+        txtPassw.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        txtPassw.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        txtPassw.setBorder(javax.swing.BorderFactory.createMatteBorder(0, 0, 2, 0, new java.awt.Color(51, 51, 255)));
+        jPanel1.add(txtPassw, new org.netbeans.lib.awtextra.AbsoluteConstraints(740, 290, 250, 40));
+
+        btnFoto.setText("Ingresa una Foto");
+        btnFoto.setFont(new java.awt.Font("Segoe UI", 0, 12)); // NOI18N
+        btnFoto.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnFotoActionPerformed(evt);
+            }
+        });
+        jPanel1.add(btnFoto, new org.netbeans.lib.awtextra.AbsoluteConstraints(740, 470, 110, 30));
+
+        jLabel13.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jLabel13.setForeground(new java.awt.Color(255, 0, 204));
+        jLabel13.setText("Segundo Apellido");
+        jPanel1.add(jLabel13, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 340, 250, 20));
+
+        txtTelefono.setBackground(new java.awt.Color(204, 255, 204));
+        txtTelefono.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        txtTelefono.setForeground(new java.awt.Color(255, 255, 255));
+        txtTelefono.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        txtTelefono.setBorder(javax.swing.BorderFactory.createMatteBorder(0, 0, 2, 0, new java.awt.Color(51, 51, 255)));
+        jPanel1.add(txtTelefono, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 460, 250, 40));
+
+        txtFirstLastName.setBackground(new java.awt.Color(204, 255, 204));
+        txtFirstLastName.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        txtFirstLastName.setForeground(new java.awt.Color(255, 255, 255));
+        txtFirstLastName.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        txtFirstLastName.setBorder(javax.swing.BorderFactory.createMatteBorder(0, 0, 2, 0, new java.awt.Color(51, 51, 255)));
+        jPanel1.add(txtFirstLastName, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 270, 250, 40));
+
+        txtName.setBackground(new java.awt.Color(204, 255, 204));
+        txtName.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        txtName.setForeground(new java.awt.Color(255, 255, 255));
+        txtName.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        txtName.setBorder(javax.swing.BorderFactory.createMatteBorder(0, 0, 2, 0, new java.awt.Color(51, 51, 255)));
+        txtName.setDisabledTextColor(new java.awt.Color(204, 255, 204));
+        txtName.setMargin(new java.awt.Insets(0, 1, 0, 0));
+        txtName.setName(""); // NOI18N
+        jPanel1.add(txtName, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 190, 250, 40));
+
+        txtUser.setBackground(new java.awt.Color(204, 255, 204));
+        txtUser.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        txtUser.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        txtUser.setBorder(javax.swing.BorderFactory.createMatteBorder(0, 0, 2, 0, new java.awt.Color(51, 51, 255)));
+        jPanel1.add(txtUser, new org.netbeans.lib.awtextra.AbsoluteConstraints(740, 190, 250, 40));
+
+        kGradientPanel1.add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 60, 1100, 710));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -170,6 +250,11 @@ public class SignUp extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btnFotoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnFotoActionPerformed
+    
+// TODO add your handling code here:
+    }//GEN-LAST:event_btnFotoActionPerformed
 
     /**
      * @param args the command line arguments
@@ -207,10 +292,13 @@ public class SignUp extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JComboBox<String> jComboBox1;
-    private com.toedter.calendar.JDateChooser jDateChooser1;
+    private keeptoo.KButton btnFoto;
+    private javax.swing.JComboBox<String> comboNacionalidad;
+    private com.toedter.calendar.JDateChooser fechaNacimiento;
     private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel11;
+    private javax.swing.JLabel jLabel12;
+    private javax.swing.JLabel jLabel13;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
@@ -220,13 +308,16 @@ public class SignUp extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel8;
     private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JPasswordField jPasswordField1;
-    private javax.swing.JPasswordField jPasswordField2;
-    private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextField3;
-    private javax.swing.JTextField jTextField4;
-    private javax.swing.JTextField jTextField5;
     private keeptoo.KButton kButton1;
     private keeptoo.KGradientPanel kGradientPanel1;
+    private javax.swing.JTextField txtCedula;
+    private javax.swing.JTextField txtEmail;
+    private javax.swing.JTextField txtFirstLastName;
+    private javax.swing.JTextField txtName;
+    private javax.swing.JPasswordField txtPassw;
+    private javax.swing.JPasswordField txtPasswConfirm;
+    private javax.swing.JTextField txtSecondLastName;
+    private javax.swing.JTextField txtTelefono;
+    private javax.swing.JTextField txtUser;
     // End of variables declaration//GEN-END:variables
 }
