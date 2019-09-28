e4c8541cd6a745a67c91f1eb114c88824651b397
steven1211 <stevenretana98@gmail.com>
2019-09-28 02:35:15 -0600

    Paquetes con procedimientos y funciones (correciones para que funcionen con las tablas)

---
+++ Packages/PKG_ADMIN_PERSON_X_COMMENT.sql
@@ -0,0 +1,58 @@
+CREATE OR REPLACE PACKAGE PKG_ADMIN_PERSON_X_COMMENT  IS
+       FUNCTION GET_ID_PERSON(PNID_COMMENT IN NUMBER)  RETURN VARCHAR2;
+       --FUNCTION GET_ID_COMMENT(PNID_PERSON IN NUMBER)  RETURN NUMBER;
+       PROCEDURE INSERT_PERSON_X_COMMENT(PNID_PERSON VARCHAR2,PNID_COMMENT NUMBER);
+       --PROCEDURE SET_ID_PERSON(NEW_VALUE NUMBER, OLD_VALUE NUMBER);
+       --PROCEDURE SET_ID_COMMENT(NEW_VALUE NUMBER, OLD_VALUE NUMBER);
+
+END PKG_ADMIN_PERSON_X_COMMENT;
+
+CREATE OR REPLACE PACKAGE BODY PKG_ADMIN_PERSON_X_COMMENT IS
+        --FUNCTION GET_ID_PERSON IMPLEMENTATION
+        FUNCTION GET_ID_PERSON(PNID_COMMENT IN NUMBER)  RETURN VARCHAR2
+         IS
+                  VNID_PERSON   VARCHAR2(30);
+         BEGIN
+                  SELECT ID_PERSON
+                  INTO VNID_PERSON
+                  FROM PERSON_X_COMMENT
+                  WHERE ID_COMMENT=PNID_COMMENT;
+                  RETURN (VNID_PERSON);
+         END;
+
+        -------------------------------------------------------------------------------
+        --FUNCTION GET_ID_COMMENT IMPLEMENTATION
+        --FUNCTION GET_ID_COMMENT(PNID_PERSON IN NUMBER) RETURN NUMBER
+        --IS
+        --         VNID_COMMENT NUMBER(20);
+
+        --BEGIN
+        --          SELECT ID_COMMENT
+        --          INTO VNID_COMMENT
+        --          FROM PERSON_X_COMMENT
+        --          WHERE ID_PERSON=PNID_PERSON;
+        --          RETURN (VNID_COMMENT);
+        --END;
+        
+        PROCEDURE INSERT_PERSON_X_COMMENT(PNID_PERSON VARCHAR2,PNID_COMMENT NUMBER) IS
+         
+           BEGIN
+             INSERT INTO PERSON_X_COMMENT(ID_PERSON, ID_COMMENT)
+             VALUES (PNID_PERSON, PNID_COMMENT);
+             
+           END INSERT_PERSON_X_COMMENT;
+           
+           
+           ---------------------------------------------------------------------------------
+    --PROCEDURE SET_ID_PERSON(NEW_VALUE NUMBER, OLD_VALUE NUMBER) IS
+    --     BEGIN
+    --         UPDATE PERSON_X_COMMENT SET ID_PERSON= NEW_VALUE WHERE ID_PERSON=OLD_VALUE;             
+    --     END SET_ID_PERSON;
+         
+         
+         ---------------------------------------------------------------------------------
+    --PROCEDURE SET_ID_COMMENT(NEW_VALUE NUMBER, OLD_VALUE NUMBER) IS
+    --     BEGIN
+    --         UPDATE PERSON_X_COMMENT SET ID_COMMENT= NEW_VALUE WHERE ID_COMMENT=OLD_VALUE;              
+    --     END SET_ID_COMMENT;
+END PKG_ADMIN_PERSON_X_COMMENT;
