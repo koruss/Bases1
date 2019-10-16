53edaa126a36095e340bfc4886f9d1cc512eafdf
Anner <anner.calvo@gmail.com>
2019-10-15 18:31:23 -0600

    Se agregan propuestas

---
+++ Scripts/12)INSERT_COMMENTS.sql
@@ -0,0 +1,151 @@
+--INSERT COMMENTS IN PROPOSAL--
+INSERT INTO PROPOSAL_COMMENT (ID_COMMENT,COMMENT_DESCRIPTION,COMMENT_DATE)
+VALUES(S_COMMENT.NEXTVAL, 'Es una muy buena idea me parece que está bien implementada la propuesta que se quiere llevar a cabo sin embargo hay que tener cuidado de que los dueños de las mascotas realmente se hagan responsables por los desechos de sus animales y no los dejen tirados en el camino porque esto puede afectar a otras personas.', SYSDATE);
+INSERT INTO PROPOSAL_X_COMMENT(ID_PROPOSAL,ID_COMMENT) VALUES(2, s_comment.currval);
+INSERT INTO PERSON_X_COMMENT(ID_COMMENT, ID_PERSON) VALUES(s_comment.currval, '701548756');
+
+INSERT INTO PROPOSAL_COMMENT (ID_COMMENT,COMMENT_DESCRIPTION,COMMENT_DATE)
+VALUES(S_COMMENT.NEXTVAL, 'Me gusta la idea sin embargo hay que tener cuidado porque en estos días hay mucho vandalismo no nos gustaría que después de hacer un esfuerzo para poder llevar a cabo este proyecto llegue alguien a dañar o a probar alguna cámara entonces me gustaría que teniendo un poco más de dinero se puede hacer una forma de resguardar las cámaras para evitar cualquier daño.', SYSDATE);
+INSERT INTO PROPOSAL_X_COMMENT(ID_PROPOSAL,ID_COMMENT) VALUES(3, s_comment.currval);
+INSERT INTO PERSON_X_COMMENT(ID_COMMENT, ID_PERSON) VALUES(s_comment.currval, '108566974');
+
+INSERT INTO PROPOSAL_COMMENT (ID_COMMENT,COMMENT_DESCRIPTION,COMMENT_DATE)
+VALUES(S_COMMENT.NEXTVAL, 'Es un gran proyecto que se quiere llevar a cabo que las personas jóvenes tienen el derecho de poder expresarse y mostrarle al mundo las capacidades que tienen y qué mejor forma de hacerlo que mediante el deporte de esta forma se evita caer en problemas con drogas o ese tipo de situaciones pudiendo llevar al país a un mejor futuro', SYSDATE);
+INSERT INTO PROPOSAL_X_COMMENT(ID_PROPOSAL,ID_COMMENT) VALUES(4, s_comment.currval);
+INSERT INTO PERSON_X_COMMENT(ID_COMMENT, ID_PERSON) VALUES(s_comment.currval, '106760070');
+
+INSERT INTO PROPOSAL_COMMENT (ID_COMMENT,COMMENT_DESCRIPTION,COMMENT_DATE)
+VALUES(S_COMMENT.NEXTVAL, 'Me gusta la idea sin embargo quiero que tengan presente que el presupuesto para poder llevar a cabo un proyecto como éste es muy alto y tal vez no logramos alcanzar lo La idea es proyectar un plan para poder llevarlo a cabo poco a poco y no sabemos a intentar resolver todos los problemas al momento porque eso afectaría más la situación en la que estamos', SYSDATE);
+INSERT INTO PROPOSAL_X_COMMENT(ID_PROPOSAL,ID_COMMENT) VALUES(11, s_comment.currval);
+INSERT INTO PERSON_X_COMMENT(ID_COMMENT, ID_PERSON) VALUES(s_comment.currval, '602547884');
+
+INSERT INTO PROPOSAL_COMMENT (ID_COMMENT,COMMENT_DESCRIPTION,COMMENT_DATE)
+VALUES(S_COMMENT.NEXTVAL, 'Yo pienso que todas las personas quieren tener su propio negocio para poder seguir adelante sin embargo no todos los negocios son factibles hay que elegir bien A qué negocio se va a apoyar y a cuál no. porque lo que se busca es una mejora del país y si se invierte dinero en un negocio que no va a funcionar Sería más bien contraproducente para el proyecto que queremos llevar a cabo', SYSDATE);
+INSERT INTO PROPOSAL_X_COMMENT(ID_PROPOSAL,ID_COMMENT) VALUES(22, s_comment.currval);
+INSERT INTO PERSON_X_COMMENT(ID_COMMENT, ID_PERSON) VALUES(s_comment.currval, '102163589');
+
+INSERT INTO PROPOSAL_COMMENT (ID_COMMENT,COMMENT_DESCRIPTION,COMMENT_DATE)
+VALUES(S_COMMENT.NEXTVAL, 'El turismo en nuestro país es la principal fuente de ingreso en las costas y en los hoteles alrededor del país que paga sus impuestos no estaría mal apoyarlos de alguna forma para poder incrementar la visibilidad de nuestro país en el extranjero haciendo que más personas estén interesadas y puedan venir a vacacionar a nuestro país', SYSDATE);
+INSERT INTO PROPOSAL_X_COMMENT(ID_PROPOSAL,ID_COMMENT) VALUES(23, s_comment.currval);
+INSERT INTO PERSON_X_COMMENT(ID_COMMENT, ID_PERSON) VALUES(s_comment.currval, '516587945');
+
+INSERT INTO PROPOSAL_COMMENT (ID_COMMENT,COMMENT_DESCRIPTION,COMMENT_DATE)
+VALUES(S_COMMENT.NEXTVAL, 'Está bien que los jóvenes tengan un lugar donde puedan reunirse e intercambiar sus conocimientos o sus habilidades y mostrárselas a las demás sin embargo llevar a cabo un proyecto como éste que tiene un presupuesto tan alto es un poco riesgoso. Esto debido a que estos centros pueden ser usados de una diferente manera en la que estamos pensando poner censados por el para contactar con otras personas y usar estos centros como una forma de centro intermediario para llevar a cabo tales acciones ilícitas.', SYSDATE);
+INSERT INTO PROPOSAL_X_COMMENT(ID_PROPOSAL,ID_COMMENT) VALUES(9, s_comment.currval);
+INSERT INTO PERSON_X_COMMENT(ID_COMMENT, ID_PERSON) VALUES(s_comment.currval, '108566974');
+
+INSERT INTO PROPOSAL_COMMENT (ID_COMMENT,COMMENT_DESCRIPTION,COMMENT_DATE)
+VALUES(S_COMMENT.NEXTVAL, 'Nuestro país siempre se ha caracterizado por invertir más en la educación de las armas por eso Estoy totalmente de acuerdo con que este proyecto se lleve a cabo buscando que más personas jóvenes puedan terminar el colegio y salir adelante haciendo que nuestro país puede avanzar a un mejor futuro y de una mejor forma', SYSDATE);
+INSERT INTO PROPOSAL_X_COMMENT(ID_PROPOSAL,ID_COMMENT) VALUES(24, s_comment.currval);
+INSERT INTO PERSON_X_COMMENT(ID_COMMENT, ID_PERSON) VALUES(s_comment.currval, '185472536');
+
+INSERT INTO PROPOSAL_COMMENT (ID_COMMENT,COMMENT_DESCRIPTION,COMMENT_DATE)
+VALUES(S_COMMENT.NEXTVAL, 'Me parece muy bien que se lleve a cabo un proyecto como éste muchas personas no tienen conciencia de lo que hacen cuando tiran su basura el más estaría de acuerdo Incluso en el que se pusiera una multa a las personas que cometen este tipo de acciones ya que realmente no están pensando en la comunidad sólo están pensando que tener que deshacerse de su basura simplemente', SYSDATE);
+INSERT INTO PROPOSAL_X_COMMENT(ID_PROPOSAL,ID_COMMENT) VALUES(6, s_comment.currval);
+INSERT INTO PERSON_X_COMMENT(ID_COMMENT, ID_PERSON) VALUES(s_comment.currval, '108450931');
+
+INSERT INTO PROPOSAL_COMMENT (ID_COMMENT,COMMENT_DESCRIPTION,COMMENT_DATE)
+VALUES(S_COMMENT.NEXTVAL, 'La expansión de la construcción urbana está afectando en sobremanera las zonas Rurales que tenemos hoy en día reduciendo el área verde con el que cuenta el país me parece bien que haya un control sobre este tipo de construcciones para que podamos seguir siendo el país verde que siempre hemos sido', SYSDATE);
+INSERT INTO PROPOSAL_X_COMMENT(ID_PROPOSAL,ID_COMMENT) VALUES(7, s_comment.currval);
+INSERT INTO PERSON_X_COMMENT(ID_COMMENT, ID_PERSON) VALUES(s_comment.currval, '119672845');
+
+INSERT INTO PROPOSAL_COMMENT (ID_COMMENT,COMMENT_DESCRIPTION,COMMENT_DATE)
+VALUES(S_COMMENT.NEXTVAL, 'Hay muchas personas en las comunidades que no sólo en esta están expuestos a riesgos sociales y estas personas pueden ser ayudados de alguna forma nosotros podemos apoyarnos haciendo ayer en eso intentando compartir con ellas los conocimientos que tenemos sobre cómo puedes llevar a cabo proyectos de avances social, por eso  apoyo el proyecto totalmente', SYSDATE);
+INSERT INTO PROPOSAL_X_COMMENT(ID_PROPOSAL,ID_COMMENT) VALUES(14, s_comment.currval);
+INSERT INTO PERSON_X_COMMENT(ID_COMMENT, ID_PERSON) VALUES(s_comment.currval, '108891423');
+
+INSERT INTO PROPOSAL_COMMENT (ID_COMMENT,COMMENT_DESCRIPTION,COMMENT_DATE)
+VALUES(S_COMMENT.NEXTVAL, 'No me gusta este proyecto es un presupuesto muy alto que podríamos usar para otro tipo de cosas más importantes no entiendo por qué le dan tanta prioridad de este tipo de cosas cuando hay cosas más preocupantes ocurriendo en la sociedad día a día', SYSDATE);
+INSERT INTO PROPOSAL_X_COMMENT(ID_PROPOSAL,ID_COMMENT) VALUES(11, s_comment.currval);
+INSERT INTO PERSON_X_COMMENT(ID_COMMENT, ID_PERSON) VALUES(s_comment.currval, '516587945');
+
+INSERT INTO PROPOSAL_COMMENT (ID_COMMENT,COMMENT_DESCRIPTION,COMMENT_DATE)
+VALUES(S_COMMENT.NEXTVAL, 'Este proyecto me agrada me parece muy bien que los jóvenes tengan un lugar donde poder ir a trabajar o es desarrollar sus habilidades cuando termine el colegio o de alguna forma están desempleados sin embargo pienso que este proyecto puede ir más allá y no sólo para las personas jóvenes sino que también para personas adultas que tienen problemas de trabajo en el momento', SYSDATE);
+INSERT INTO PROPOSAL_X_COMMENT(ID_PROPOSAL,ID_COMMENT) VALUES(18, s_comment.currval);
+INSERT INTO PERSON_X_COMMENT(ID_COMMENT, ID_PERSON) VALUES(s_comment.currval, '108566974');
+
+INSERT INTO PROPOSAL_COMMENT (ID_COMMENT,COMMENT_DESCRIPTION,COMMENT_DATE)
+VALUES(S_COMMENT.NEXTVAL, 'Nadie tiene por qué ser maltratado por pensar obesa diferente y a pesar de que pienso que estos educación que se da desde la casa no está mal reforzar este tipo de actitudes en las instituciones haciendo ver a los niños que toda persona diferente y que no por eso tiene que ser tratado de diferente manera', SYSDATE);
+INSERT INTO PROPOSAL_X_COMMENT(ID_PROPOSAL,ID_COMMENT) VALUES(20, s_comment.currval);
+INSERT INTO PERSON_X_COMMENT(ID_COMMENT, ID_PERSON) VALUES(s_comment.currval, '108566974');
+
+INSERT INTO PROPOSAL_COMMENT (ID_COMMENT,COMMENT_DESCRIPTION,COMMENT_DATE)
+VALUES(S_COMMENT.NEXTVAL, 'Hacer deporte es una forma de decir quitarse el peso del día y eso no sólo para las personas jóvenes en las personas adultas también y niños que simplemente Quiero jugar por eso apoyo la propuesta totalmente sin embargo quiero que se tenga en cuenta que no tenemos un presupuesto para cubrir todas las necesidades de que un proyecto Así que quiera llevarse a cabo', SYSDATE);
+INSERT INTO PROPOSAL_X_COMMENT(ID_PROPOSAL,ID_COMMENT) VALUES(13, s_comment.currval);
+INSERT INTO PERSON_X_COMMENT(ID_COMMENT, ID_PERSON) VALUES(s_comment.currval, '117060279');
+
+INSERT INTO PROPOSAL_COMMENT (ID_COMMENT,COMMENT_DESCRIPTION,COMMENT_DATE)
+VALUES(S_COMMENT.NEXTVAL, 'Se debe de estudiar no solo desde el ámbito biológico, no solo los procesos que suceden los cambios hormonales, sino también desde el  área socio-afectiva', SYSDATE);
+INSERT INTO PROPOSAL_X_COMMENT(ID_PROPOSAL,ID_COMMENT) VALUES(16, s_comment.currval);
+INSERT INTO PERSON_X_COMMENT(ID_COMMENT, ID_PERSON) VALUES(s_comment.currval, '117060279');
+
+INSERT INTO PROPOSAL_COMMENT (ID_COMMENT,COMMENT_DESCRIPTION,COMMENT_DATE)
+VALUES(S_COMMENT.NEXTVAL, 'Todas las personas tiene que saber los derechos que se tiene por ser persona y hasta dónde llegan estos derechos para no invadir a los de los demás y no entran en conflicto tanto personales como legales con estas personas y está bueno que se enseñe desde pequeño a las personas a comprender este tipo de temas que cuando llegan a ser adultos y no tiene ninguna base puede ser más complicado comprender', SYSDATE);
+INSERT INTO PROPOSAL_X_COMMENT(ID_PROPOSAL,ID_COMMENT) VALUES(19, s_comment.currval);
+INSERT INTO PERSON_X_COMMENT(ID_COMMENT, ID_PERSON) VALUES(s_comment.currval, '108450931');
+
+INSERT INTO PROPOSAL_COMMENT (ID_COMMENT,COMMENT_DESCRIPTION,COMMENT_DATE)
+VALUES(S_COMMENT.NEXTVAL, 'Es importante ya que es un problema que está causando muchas deserciones en las escuelas, será de mucho bienestar para la sociedad y la comunidad educativa si se imparte la educación sexual como materia en las escuelas.La educación sexual, incluyendo sus aspectos espirituales, debe ser parte de una amplia en salud y moral, desde kínder Garden hasta el doceavo grado, idealmente llevada a cabo armoniosamente por padres y maestros.', SYSDATE);
+INSERT INTO PROPOSAL_X_COMMENT(ID_PROPOSAL,ID_COMMENT) VALUES(16, s_comment.currval);
+INSERT INTO PERSON_X_COMMENT(ID_COMMENT, ID_PERSON) VALUES(s_comment.currval, '106760070');
+
+INSERT INTO PROPOSAL_COMMENT (ID_COMMENT,COMMENT_DESCRIPTION,COMMENT_DATE)
+VALUES(S_COMMENT.NEXTVAL, 'Estar en riesgo de pobreza y exclusión social influye negativamente en el estado de salud, algo que se refleja en la mayor incidencia de enfermedades graves y crónicas. También incide en las dificultades económicas para la atención de la propia salud. De hecho, el 35.1% ha tenido dificultades económicas para cubrir gastos relacionados con su salud en el último año. En este sentido, las dificultades más frecuentes son los problemas para pagar medicinas, por eso este proyecto es tan importante', SYSDATE);
+INSERT INTO PROPOSAL_X_COMMENT(ID_PROPOSAL,ID_COMMENT) VALUES(14, s_comment.currval);
+INSERT INTO PERSON_X_COMMENT(ID_COMMENT, ID_PERSON) VALUES(s_comment.currval, '144785623');
+
+INSERT INTO PROPOSAL_COMMENT (ID_COMMENT,COMMENT_DESCRIPTION,COMMENT_DATE)
+VALUES(S_COMMENT.NEXTVAL, 'Mejora la seguridad de las personas que por ahí pasen ya que al tener cámara se puede reducir el vandalismo y la delincuencia.', SYSDATE);
+INSERT INTO PROPOSAL_X_COMMENT(ID_PROPOSAL,ID_COMMENT) VALUES(3, s_comment.currval);
+INSERT INTO PERSON_X_COMMENT(ID_COMMENT, ID_PERSON) VALUES(s_comment.currval, '144785623');
+
+INSERT INTO PROPOSAL_COMMENT (ID_COMMENT,COMMENT_DESCRIPTION,COMMENT_DATE)
+VALUES(S_COMMENT.NEXTVAL, 'La escuela debe enseñarles a los niños y estudiantes en general el a,b,c y el 1,2,3 cosas así, es decir cosas útiles para sus vidas para que puedan defenderse en la vida y elegir una profesión u oficio que les permita ganarse el sustento, ese es el fin último de la educación, lo demás son cosas superfluas. Aparte ¿por qué el estado debe ser quien monopolice la educación?', SYSDATE);
+INSERT INTO PROPOSAL_X_COMMENT(ID_PROPOSAL,ID_COMMENT) VALUES(21, s_comment.currval);
+INSERT INTO PERSON_X_COMMENT(ID_COMMENT, ID_PERSON) VALUES(s_comment.currval, '117060279');
+
+INSERT INTO PROPOSAL_COMMENT (ID_COMMENT,COMMENT_DESCRIPTION,COMMENT_DATE)
+VALUES(S_COMMENT.NEXTVAL, 'El reconocimiento y la aceptación de algo tan troncal como la identidad sexual y la propia orientación sexual son el a, b, c, de cosas útiles para la vida. Y conocer los mecanismos para la defensa de sus derechos es el 1, 2, 3, para defenderse en la vida. Ninguno de estos asuntos son cosas superfluas. En cuanto a la monopolización de la educación por parte del Estado: no hay tal. La prueba es la proliferación de instituciones educativas privadas en todos los niveles del sistema educativo. Lo que sí hay es un monopolio de vigilancia y control del servicio público de educación, aunque lo presten entidades privadas. Esto es propio de la potestad de todo Estado.', SYSDATE);
+INSERT INTO PROPOSAL_X_COMMENT(ID_PROPOSAL,ID_COMMENT) VALUES(21, s_comment.currval);
+INSERT INTO PERSON_X_COMMENT(ID_COMMENT, ID_PERSON) VALUES(s_comment.currval, '119672845');
+
+INSERT INTO PROPOSAL_COMMENT (ID_COMMENT,COMMENT_DESCRIPTION,COMMENT_DATE)
+VALUES(S_COMMENT.NEXTVAL, 'Es una buena manera de que las personas se integren a otros grupos y no estén sumidos en su propio mundo que busque ayuda y compartir con demás personas que sea necesario para poder crecer integralmente', SYSDATE);
+INSERT INTO PROPOSAL_X_COMMENT(ID_PROPOSAL,ID_COMMENT) VALUES(15, s_comment.currval);
+INSERT INTO PERSON_X_COMMENT(ID_COMMENT, ID_PERSON) VALUES(s_comment.currval, '117060279');
+
+INSERT INTO PROPOSAL_COMMENT (ID_COMMENT,COMMENT_DESCRIPTION,COMMENT_DATE)
+VALUES(S_COMMENT.NEXTVAL, 'Las calles de esta comunidad es auténtico desastre hay que hacer para regular el tránsito y todo el medio que se muere en las calles esto debido a que muchos niños que se mueven en las calles que van para sus escuelas y personas mayores que pueden estar en riesgo de un atropello o una mala situación y no sólo estás tan jóvenes y adultos que se dirigen hacia sus trabajos', SYSDATE);
+INSERT INTO PROPOSAL_X_COMMENT(ID_PROPOSAL,ID_COMMENT) VALUES(8, s_comment.currval);
+INSERT INTO PERSON_X_COMMENT(ID_COMMENT, ID_PERSON) VALUES(s_comment.currval, '206745284');
+
+INSERT INTO PROPOSAL_COMMENT (ID_COMMENT,COMMENT_DESCRIPTION,COMMENT_DATE)
+VALUES(S_COMMENT.NEXTVAL, 'Buscar apoyar a los jóvenes deben ser una prioridad es para cualquier gobierno y una de las mejores formas de hacerlo es promoviendo el deporte donde ellos se pueden desempeñar y mostrar las habilidades que tienen por eso apoyar proyectos que tengan esa finalidad siempre va a ser una ganancia para el país y para las comunidades en donde se lleve a cabo', SYSDATE);
+INSERT INTO PROPOSAL_X_COMMENT(ID_PROPOSAL,ID_COMMENT) VALUES(4, s_comment.currval);
+INSERT INTO PERSON_X_COMMENT(ID_COMMENT, ID_PERSON) VALUES(s_comment.currval, '108450931');
+
+INSERT INTO PROPOSAL_COMMENT (ID_COMMENT,COMMENT_DESCRIPTION,COMMENT_DATE)
+VALUES(S_COMMENT.NEXTVAL, 'No puede ser posible que cualquier persona pueda construir donde prácticamente quiera sin pensar en los riesgos ambientales y económicos que pueden ocasionar no sólo a la comunidad sino incluso a ellos mismos sino se plantea bien la situación en la que se quiere construir', SYSDATE);
+INSERT INTO PROPOSAL_X_COMMENT(ID_PROPOSAL,ID_COMMENT) VALUES(7, s_comment.currval);
+INSERT INTO PERSON_X_COMMENT(ID_COMMENT, ID_PERSON) VALUES(s_comment.currval, '117060279');
+
+INSERT INTO PROPOSAL_COMMENT (ID_COMMENT,COMMENT_DESCRIPTION,COMMENT_DATE)
+VALUES(S_COMMENT.NEXTVAL, 'Las personas no pueden construir donde quieran necesitan el permiso para poder hacer cualquier tipo de construcción ya sea en sus casas o en un terreno no pueden hacerlo por deseo propio tiene que tener un respaldo de una institución que asegure que puede llevarse a cabo cualquier tipo de construcción, reparación o mantenimiento', SYSDATE);
+INSERT INTO PROPOSAL_X_COMMENT(ID_PROPOSAL,ID_COMMENT) VALUES(7, s_comment.currval);
+INSERT INTO PERSON_X_COMMENT(ID_COMMENT, ID_PERSON) VALUES(s_comment.currval, '108450931');
+
+INSERT INTO PROPOSAL_COMMENT (ID_COMMENT,COMMENT_DESCRIPTION,COMMENT_DATE)
+VALUES(S_COMMENT.NEXTVAL, 'La cultura y la historia el que nos forja como nación y preservar estas estructuras como patrimonios de nuestra comunidad es de suma importancia para mostrarle al mundo que somos un país con identidad propia y que no necesita copiarse de nadie más', SYSDATE);
+INSERT INTO PROPOSAL_X_COMMENT(ID_PROPOSAL,ID_COMMENT) VALUES(11, s_comment.currval);
+INSERT INTO PERSON_X_COMMENT(ID_COMMENT, ID_PERSON) VALUES(s_comment.currval, '701548756');
+
+INSERT INTO PROPOSAL_COMMENT (ID_COMMENT,COMMENT_DESCRIPTION,COMMENT_DATE)
+VALUES(S_COMMENT.NEXTVAL, 'Se debe intentar ayudar a las personas que quieren crear su propia empresa que tienen un plan definido y que sabe cómo llevar a cabo estas personas son las que podrían hacer que nuestro país hay adelante ya que diversificar y en la economía y no nos vamos a estancar en sólo un tipo de producto a ofrecer al mundo', SYSDATE);
+INSERT INTO PROPOSAL_X_COMMENT(ID_PROPOSAL,ID_COMMENT) VALUES(22, s_comment.currval);
+INSERT INTO PERSON_X_COMMENT(ID_COMMENT, ID_PERSON) VALUES(s_comment.currval, '102163589');
+
+INSERT INTO PROPOSAL_COMMENT (ID_COMMENT,COMMENT_DESCRIPTION,COMMENT_DATE)
+VALUES(S_COMMENT.NEXTVAL, 'Da la posibilidad de nuevos empleos, sin embargo también puede traer repercusiones negativas como el aumento de la cantidad de bares, casinos y prostíbulos. Se requiere una gran cooperación del sector turístico con el estado para que una economía basada en el turismo de frutos', SYSDATE);
+INSERT INTO PROPOSAL_X_COMMENT(ID_PROPOSAL,ID_COMMENT) VALUES(23, s_comment.currval);
+INSERT INTO PERSON_X_COMMENT(ID_COMMENT, ID_PERSON) VALUES(s_comment.currval, '516587945');
+
